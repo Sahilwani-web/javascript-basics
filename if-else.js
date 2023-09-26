f2c09dc44c7092f0990c711c210e80f12f480055
Sahil wani <123382834+Sahilwani-web@users.noreply.github.com>
2023-09-26 09:00:18 +0000

    switch case

--- if-else.js
+++ if-else.js
@@ -1,13 +1,25 @@
-let isCorrectGuess = false;
-const actualNumber = 3;
-const myGuess = prompt('Please enter a number between 1 and 5');
+// let isCorrectGuess = false;
+// const actualNumber = 3;
+// const myGuess = prompt('Please enter a number between 1 and 5');
 
-if (myGuess == actualNumber) {
-    isCorrectGuess = true;
-}
+// if (myGuess == actualNumber) {
+//     isCorrectGuess = true;
+// }
 
-if (isCorrectGuess == true) {
-    console.log("Great you have entered correct number");
-} else {
-    console.log(`sorry, the actual number is ${actualNumber}`);
-}
+// if (isCorrectGuess == true) {
+//     console.log("Great you have entered correct number");
+// } else {
+//     console.log(`sorry, the actual number is ${actualNumber}`);
+// }
+
+const userLogged = true
+const debitCard = true
+
+const loggedFromGmail = false
+const loggedFromEmail = true
+if (userLogged && debitCard) {
+    console.log("Shopping is allowed")
+};
+if (loggedFromEmail || loggedFromGmail) {
+    console.log("login is allowed")
+};
