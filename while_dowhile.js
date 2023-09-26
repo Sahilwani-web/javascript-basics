a4f0ff532c24a5825f735346f44f8ba3fc4228bf
Sahil wani <123382834+Sahilwani-web@users.noreply.github.com>
2023-09-26 09:17:51 +0000

    while and dowhile

---
+++ while_dowhile.js
@@ -0,0 +1,21 @@
+// while
+
+let index = 0
+while (index<=10) {
+    console.log(`value is ${index}`)
+    index = index + 2;
+};
+
+let myArr = ["sahil", "Mughes", "Bintu"]
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`value is ${myArr[arr]}`);
+    arr++
+};
+
+// do while
+let score = 1
+do {
+    console.log(`score is ${score}`)
+      score + 2
+} while (score <=10);
